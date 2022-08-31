0d02c8a02a9c9c98d814055da31479e35a758a54
AleksandrDorin <dorinaleksandrspb@yandex.ru>
2022-08-31 22:04:55 +0300

    Task04 done

---
+++ Task04/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+void Arrange(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+}
+
+int[,] matrix = new int[5, 5];
+Console.WriteLine("Исходный массив: ");
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("");
+Console.WriteLine("Измененный массив: ");
+Arrange(matrix);
+PrintMatrix(matrix);
+
